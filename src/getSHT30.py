bf392b72700dd6f6c1871a3c7032782795881899
Gavin Purcell <mkorangestripe@gmail.com>
2025-08-12 16:09:53 -0500

    Update getSHT30.py to use config/getSHT30.json, improve exception handling

--- src/getSHT30.py
+++ src/getSHT30.py
@@ -10,13 +10,17 @@ import struct
 import time
 from dotenv import load_dotenv
 import smbus
-from influxdb.exceptions import InfluxDBServerError
+from requests.exceptions import Timeout
+from requests.exceptions import ConnectionError as RequestsConnectionError
+from influxdb.exceptions import InfluxDBServerError, InfluxDBClientError
 from constants import HUMIDITY_TEMP_SENSOR_TYPE as SENSOR_TYPE
-from common_functions import database_connect
+from common_functions import database_connect, load_json_file
 
 DEBUG = False  # set to True to print query result
 
 HOSTNAME = socket.gethostname()
+CONFIG_FILE = "config/getSHT30.json"
+CONFIG_FILE_TRY_AGAIN_SECS = 60
 
 if 'INVOCATION_ID' in os.environ:
     print(f"Running under Systemd, using .env.{HOSTNAME} file")
@@ -31,10 +35,6 @@ USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 DATABASE = os.getenv("SENSOR_DATABASE")
 
-SENSOR_LOCATION = os.getenv("HUMIDITY_TEMP_SENSOR_LOCATION")
-SENSOR_ID = os.getenv("HUMIDITY_TEMP_SENSOR_ID")
-SENSOR_TITLE = os.getenv("HUMIDITY_TEMP_SENSOR_TITLE")
-
 I2C_ADDR = 0x44
 WRITE_REGISTER = 0x2C
 READ_REGISTER = 0x00
@@ -46,6 +46,34 @@ db_client = database_connect(INFLUXDB_HOST, INFLUXDB_PORT, USERNAME, PASSWORD, D
 bus = smbus.SMBus(1)
 
 while True:
+
+    print("Loading config file")
+    SENSORS = load_json_file(CONFIG_FILE).get(HOSTNAME)
+    if SENSORS is None:
+        print(f"Hostname not found in {CONFIG_FILE}")
+        print(f"Trying again in {CONFIG_FILE_TRY_AGAIN_SECS} seconds")
+        time.sleep(CONFIG_FILE_TRY_AGAIN_SECS)
+        continue
+
+    sensor_count = len(SENSORS)
+
+    if sensor_count == 0:
+        print(f"No sensors for {HOSTNAME} found in {CONFIG_FILE}")
+        print(f"Trying again in {CONFIG_FILE_TRY_AGAIN_SECS} seconds")
+        time.sleep(CONFIG_FILE_TRY_AGAIN_SECS)
+        continue
+
+    if sensor_count > 1:
+        print(f"More than one sensor found for {HOSTNAME} in {CONFIG_FILE}")
+        print("Not currently handling multiple SHT30 sensors per host")
+        print(f"Trying again in {CONFIG_FILE_TRY_AGAIN_SECS} seconds")
+        time.sleep(CONFIG_FILE_TRY_AGAIN_SECS)
+        continue
+
+    SENSOR_LOCATION = list(SENSORS.keys())[0]
+    SENSOR_ID = SENSORS[SENSOR_LOCATION]["id"]
+    SENSOR_TITLE = SENSORS[SENSOR_LOCATION]["title"]
+
     series = []
     print("Reading SHT30 sensors")
 
@@ -105,7 +133,8 @@ while True:
             query_result = db_client.query('SELECT * FROM "temps" WHERE time >= now() - 10s')
             print(f"Query results: {query_result}")
 
-    except InfluxDBServerError as e:
+    except (InfluxDBServerError, InfluxDBClientError, RequestsConnectionError, Timeout) as e:
         print("Failure writing to or reading from InfluxDB:", e)
+        db_client = database_connect(INFLUXDB_HOST, INFLUXDB_PORT, USERNAME, PASSWORD, DATABASE)
 
     time.sleep(10)
